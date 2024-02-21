8c6b955a3c517998f5705f796c7d9920684488f1
JJozef <Josemillaquen12@gmail.com>
2024-02-21 00:23:40 -0300

    Documentation added to the functions

--- src/lib/function.js
+++ src/lib/function.js
@@ -1,5 +1,13 @@
 import { encode, decode } from 'js-base64'
 
+/**
+ * - Generate the HTML
+ * @param {string} html
+ * @param {string} css
+ * @param {string} javascript
+ * @returns {string} The HTML string
+ */
+
 export function GenerateHTML({ html, css, javascript }) {
   return `
     <!DOCTYPE html>
@@ -19,14 +27,34 @@ export function GenerateHTML({ html, css, javascript }) {
     `
 }
 
+/**
+ * - Generate a hashed URL
+ * @param {string} html
+ * @param {string} css
+ * @param {string} javascript
+ * @returns {string} The hashed URL
+ */
+
 export function GenerateURLHashed({ html, css, javascript }) {
   return `${encode(html || '')}|${encode(css || '')}|${encode(javascript || '')}`
 }
+
+/**
+ * - Decode the URL
+ * @param {string} encoded
+ * @returns {string} The decoded URL
+ */
 
 export function DecodeURLHashed(encoded) {
   if (!encoded) return ''
   return decode(encoded)
 }
+
+/**
+ * - Replace the current state with a new state
+ * @param {string} url
+ * @returns {void}
+ */
 
 export const ReplaceStateURL = (url) => {
   return window.history.replaceState(
